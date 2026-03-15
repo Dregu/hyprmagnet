a0f9292a14be6102ac4c8e60f41a1a8e60df6aea
Dregu <dregu@dreg.us>
2026-03-15 08:37:06 +0200

    add smart monitor warping

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # hyprmagnet
 
-Plugin to move mouse events from just outside a tiled window (i.e. from outer gaps and borders) to just inside the window, to click browser tabs and scrollbars without having to aim inside the window, for Hyprland 0.53+.
+Hyprland plugin with some smart mouse hooks, for Hyprland 0.53+.
+
+- move mouse events from just outside a tiled window (i.e. from outer gaps and borders) to just inside the window, to click browser tabs and scrollbars without having to aim inside the window
+- smarter warp to nearby monitors when bonking non-intersecting monitor edges
 
 ```ini
 # Enabled per window with dynamic tag magnet
@@ -8,8 +11,12 @@ windowrule = match:class firefox|codium, tag +magnet
 
 plugin {
     magnet {
-        edge = tr # Window edges to enable on, any combination of t/b/l/r (default: t)
-        pad  = 3  # Padding inside the window if your tabs aren't quite at the edge (default: 0)
+        edge = tr   # Window edges to enable on, any combo of t/b/l/r (default: t)
+        pad  = 3    # Pad inside the window if your tabs aren't quite at the edge (default: 0)
+        warp = ig   # Warp flags when bonking monitor edges, any combo of i/g (default: none)
+                    # i: warp to nearest intersection, g: cross gaps in monitor layout
+                    # Order matters; ig prefers intersecting monitors and gi prefers gaps
+        delay = 500 # Cooldown between monitor warps, ms (default: 500)
     }
 }
 ```
--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 
+#include <hyprland/src/helpers/math/Direction.hpp>
 #include <unistd.h>
 #include <string>
 
@@ -11,6 +12,7 @@
 #include <hyprland/src/plugins/PluginAPI.hpp>
 #include <hyprland/src/config/ConfigManager.hpp>
 #include <hyprland/src/layout/target/Target.hpp>
+#include <hyprland/src/helpers/Monitor.hpp>
 
 inline HANDLE         PHANDLE = nullptr;
 
@@ -21,31 +23,107 @@ APICALL EXPORT std::string PLUGIN_API_VERSION() {
     return HYPRLAND_API_VERSION;
 }
 
-void hkMouse(CInputManager* thisptr, uint32_t time, bool refocus, bool mouse, std::optional<Vector2D> overridePos) {
-    if (g_pInputManager->m_lastFocusOnLS) {
-        (*(origMotion)g_pMouseHook->m_original)(thisptr, time, refocus, mouse, overridePos);
-        return;
+CTimer     g_warped;
+
+PHLMONITOR getMonitorAcrossGap(Math::eDirection dir, bool inter) {
+    const auto ACTIVE   = Desktop::focusState()->monitor();
+    const auto COORDS   = g_pPointerManager->position();
+    float      bestDist = __FLT_MAX__;
+    PHLMONITOR bestMon;
+    for (auto& mon : g_pCompositor->m_monitors) {
+        const auto POINT = mon->logicalBox().closestPoint(COORDS);
+        const auto DIST  = (dir == Math::DIRECTION_LEFT || dir == Math::DIRECTION_RIGHT) ? abs(COORDS.y - POINT.y) : abs(COORDS.x - POINT.x);
+        if (DIST < bestDist) {
+            bool INTERSECTV = !((mon->m_position.y + mon->m_size.y < ACTIVE->m_position.y) || (ACTIVE->m_position.y + ACTIVE->m_size.y < mon->m_position.y));
+            bool INTERSECTH = !((mon->m_position.x + mon->m_size.x < ACTIVE->m_position.x) || (ACTIVE->m_position.x + ACTIVE->m_size.x < mon->m_position.x));
+            if (!inter) {
+                INTERSECTV = INTERSECTV && COORDS.y > mon->m_position.y && COORDS.y < mon->m_position.y + mon->m_size.y;
+                INTERSECTH = INTERSECTH && COORDS.x > mon->m_position.x && COORDS.x < mon->m_position.x + mon->m_size.x;
+            }
+            if ((dir == Math::DIRECTION_LEFT && mon->logicalBox().middle().x < COORDS.x && INTERSECTV) ||
+                (dir == Math::DIRECTION_RIGHT && mon->logicalBox().middle().x > COORDS.x && INTERSECTV) ||
+                (dir == Math::DIRECTION_UP && mon->logicalBox().middle().y < COORDS.y && INTERSECTH) ||
+                (dir == Math::DIRECTION_DOWN && mon->logicalBox().middle().y > COORDS.y && INTERSECTH)) {
+                bestDist = DIST;
+                bestMon  = mon;
+            }
+        }
     }
+    return bestMon;
+}
+
+void hkMouse(CInputManager* thisptr, uint32_t time, bool refocus, bool mouse, std::optional<Vector2D> overridePos) {
     static auto PEDGE    = CConfigValue<std::string>("plugin:magnet:edge");
     static auto PPAD     = CConfigValue<Hyprlang::INT>("plugin:magnet:pad");
     static auto PGAPSOUT = CConfigValue<Hyprlang::CUSTOMTYPE>("general:gaps_out");
     static auto PBORDER  = CConfigValue<Hyprlang::INT>("general:border_size");
 
-    const auto* GAPSOUT = sc<CCssGapData*>(PGAPSOUT.ptr()->getData());
-    const auto  PAD     = -sc<double>(*PPAD);
-    const auto  COORDS  = g_pPointerManager->position();
-    const auto  WIN     = Desktop::focusState()->window();
-    const auto  NEWWIN  = g_pCompositor->vectorToWindowUnified(COORDS, Desktop::View::RESERVED_EXTENTS | Desktop::View::INPUT_EXTENTS | Desktop::View::ALLOW_FLOATING);
+    static auto PWARP    = CConfigValue<std::string>("plugin:magnet:warp");
+    static auto PDELAY   = CConfigValue<Hyprlang::INT>("plugin:magnet:delay");
+    static auto PHOTSPOT = CConfigValue<Hyprlang::INT>("cursor:hotspot_padding");
+
+    const auto* GAPSOUT       = sc<CCssGapData*>(PGAPSOUT.ptr()->getData());
+    const auto  PAD           = -sc<double>(*PPAD);
+    const auto  COORDS        = g_pPointerManager->position();
+    const auto  COORDSFLOORED = COORDS.floor();
+    const auto  MON           = Desktop::focusState()->monitor();
+    const auto  WIN           = Desktop::focusState()->window();
+    const auto  NEWWIN        = g_pCompositor->vectorToWindowUnified(COORDS, Desktop::View::RESERVED_EXTENTS | Desktop::View::INPUT_EXTENTS | Desktop::View::ALLOW_FLOATING);
+
+    if (MON && g_warped.getMillis() > *PDELAY && !(*PWARP).empty()) {
+        const auto LOCAL  = COORDSFLOORED - MON->m_position;
+        auto       BOX    = MON->logicalBox().expand(-(*PHOTSPOT) - 1);
+        auto       MID    = BOX.middle();
+        bool       BONKED = !BOX.containsPoint(COORDSFLOORED);
+        if (BONKED) {
+            Math::eDirection DIR{Math::DIRECTION_DEFAULT};
+            if (BOX.containsPoint(COORDSFLOORED + Vector2D(1, 0)) && COORDS.x < MID.x)
+                DIR = Math::DIRECTION_LEFT;
+            else if (BOX.containsPoint(COORDSFLOORED + Vector2D(-1, 0)) && COORDS.x > MID.x)
+                DIR = Math::DIRECTION_RIGHT;
+            else if (BOX.containsPoint(COORDSFLOORED + Vector2D(0, 1)) && COORDS.y < MID.y)
+                DIR = Math::DIRECTION_UP;
+            else if (BOX.containsPoint(COORDSFLOORED + Vector2D(0, -1)) && COORDS.y > MID.y)
+                DIR = Math::DIRECTION_DOWN;
+            if (DIR != Math::DIRECTION_DEFAULT) {
+                const bool INTER = (*PWARP).contains('i');
+                for (char c : *PWARP) {
+                    if (c == 'i') {
+                        if (auto NEWMON = g_pCompositor->getMonitorInDirection(DIR); NEWMON) {
+                            auto NEWPOS = NEWMON->logicalBox().closestPoint(COORDS);
+                            Log::logger->log(Log::DEBUG, "[hyprmagnet] INT WARP TO {} {}", NEWMON->m_name, NEWPOS);
+                            g_warped.reset();
+                            g_pPointerManager->warpTo(NEWPOS);
+                            return;
+                        }
+                    } else if (c == 'g') {
+                        if (auto NEWMON = getMonitorAcrossGap(DIR, INTER); NEWMON) {
+                            auto NEWPOS = NEWMON->logicalBox().closestPoint(COORDS);
+                            Log::logger->log(Log::DEBUG, "[hyprmagnet] GAP WARP TO {} {}", NEWMON->m_name, NEWPOS);
+                            g_warped.reset();
+                            g_pPointerManager->warpTo(NEWPOS);
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    if (g_pInputManager->m_lastFocusOnLS) {
+        (*(origMotion)g_pMouseHook->m_original)(thisptr, time, refocus, mouse, overridePos);
+        return;
+    }
 
     if (WIN && (!NEWWIN || WIN == NEWWIN) && WIN->m_ruleApplicator->m_tagKeeper.isTagged("magnet")) {
         const auto WORKSPACERULE = g_pConfigManager->getWorkspaceRuleFor(WIN->layoutTarget()->workspace());
         auto       GAPS          = WORKSPACERULE.gapsOut.value_or(*GAPSOUT);
         auto       BOX           = WIN->getWindowMainSurfaceBox();
         auto       GAPBOX        = BOX.copy();
-        auto       LEFT          = (*PEDGE).contains("l");
-        auto       RIGHT         = (*PEDGE).contains("r");
-        auto       TOP           = (*PEDGE).contains("t");
-        auto       BOTTOM        = (*PEDGE).contains("b");
+        auto       LEFT          = (*PEDGE).contains('l');
+        auto       RIGHT         = (*PEDGE).contains('r');
+        auto       TOP           = (*PEDGE).contains('t');
+        auto       BOTTOM        = (*PEDGE).contains('b');
         GAPBOX.addExtents(SBoxExtents{
             Vector2D{LEFT ? (int)GAPS.m_left : 0, TOP ? (int)GAPS.m_top : 0},
             Vector2D{RIGHT ? (int)GAPS.m_right : 0, BOTTOM ? (int)GAPS.m_bottom : 0},
@@ -73,6 +151,8 @@ APICALL EXPORT PLUGIN_DESCRIPTION_INFO PLUGIN_INIT(HANDLE handle) {
 
     HyprlandAPI::addConfigValue(PHANDLE, "plugin:magnet:pad", Hyprlang::INT{0});
     HyprlandAPI::addConfigValue(PHANDLE, "plugin:magnet:edge", Hyprlang::STRING{"t"});
+    HyprlandAPI::addConfigValue(PHANDLE, "plugin:magnet:warp", Hyprlang::STRING{""});
+    HyprlandAPI::addConfigValue(PHANDLE, "plugin:magnet:delay", Hyprlang::INT{500});
 
     auto FNS = HyprlandAPI::findFunctionsByName(PHANDLE, "mouseMoveUnified");
     for (auto& fn : FNS) {
@@ -96,7 +176,7 @@ APICALL EXPORT PLUGIN_DESCRIPTION_INFO PLUGIN_INIT(HANDLE handle) {
         throw std::runtime_error("[hyprmagnet] Hook failed");
     }
 
-    return {"hyprmagnet", "Move mouse events from gaps to nearby windows", "Dregu", "0.2.0"};
+    return {"hyprmagnet", "Move mouse events from gaps to nearby windows and warp between monitors", "Dregu", "0.3.0"};
 }
 
 APICALL EXPORT void PLUGIN_EXIT() {
